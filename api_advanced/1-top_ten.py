055089aebfb8e3e1dce2e3ff446de6cf2f8d2bb2
cyuzuzo-aime <a.cyuzuzo2@alustudent.com>
2025-07-15 12:47:55 +0200

    updated filtering sub reddits

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-""" Get the titles of the first 10 hot posts for a given subreddit."""
+"""Get the titles of the first 10 hot posts for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
 
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
-    else:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+    except requests.RequestException:
+        print(None)
+        return
+
+    if response.status_code != 200:
+        print(None)
+        return
+
+    try:
+        posts = response.json().get("data", {}).get("children", [])
+        for post in posts:
+            print(post.get("data", {}).get("title"))
+    except Exception:
         print(None)
