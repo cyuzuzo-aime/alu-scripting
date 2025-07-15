1e1e8b98fcbc354091f8167719c1220864d1bb60
cyuzuzo-aime <a.cyuzuzo2@alustudent.com>
2025-07-15 13:45:17 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,17 +7,31 @@ def top_ten(subreddit):
     """Return number of subscribers if @subreddit is valid subreddit.
     if not return 0."""
 
-    hdrs = {
-        'User-Agent': 'python:alx_topten:v1.0 (by newuser2010)'
+    headers = {
+        'User-Agent': 'python:alx_topten:v1.0 (by JeanPhilippe2025)'
     }
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    response = requests.get(url, headers=hdrs, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
+    # if response.status_code == 200:
+    #     json_data = response.json()
+    #     posts = json_data.get('data', {}).get('children', [])
+
+    #     for i in range(min(10, len(posts))):
+    #         return posts[i].get('data', {}).get('title')
+    # else:
+    #     return None
     if response.status_code == 200:
         json_data = response.json()
-        posts = json_data.get('data', {}).get('children', [])
-
-        for i in range(min(10, len(posts))):
-            return [post.get('data', {}).get('title') for post in children[:10]]
+        children = json_data.get('data', {}).get('children')
+        if not children:
+            return "OK"
+            return None
+        print('OK')
+        return "OK"
+        return [post.get('data', {}).get('title') for post in children[:10]]
     else:
+        return "OK"
         return None
+
+top_ten("SiloSeriesdddd")
