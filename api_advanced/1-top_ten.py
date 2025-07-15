b4f38edafc01b7da304f03a967db6759a5226fbf
cyuzuzo-aime <a.cyuzuzo2@alustudent.com>
2025-07-15 12:28:34 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
+""" Get the titles of the first 10 hot posts for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.
-    if not return 0."""
 
-    hdrs = {
-        'User-Agent': 'python:alx_topten:v1.0 (by newuser20)'
-    }
-    s_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    res = requests.get(s_url, headers=hdrs, allow_redirects=False)
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
 
-    if res.status_code == 200:
-        json_data = res.json()
-        posts = json_data.get('data', {}).get('children', [])
-
-        for i in range(min(10, len(posts))):
-            print(posts[i].get('data', {}).get('title'))
+    if response.status_code == 200:
+        json_data = response.json()
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
     else:
         print(None)
