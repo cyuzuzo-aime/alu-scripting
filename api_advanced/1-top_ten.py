32d1cac3b815080f8b8b5bd925364864fff739ae
cyuzuzo-aime <a.cyuzuzo2@alustudent.com>
2025-07-15 14:48:08 +0200

    Updated 1-top_ten.py again

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,30 +7,17 @@ def top_ten(subreddit):
     """Return number of subscribers if @subreddit is valid subreddit.
     if not return 0."""
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-
     headers = {
-        "User-Agent": "scripting/1.0 (by /u/user0221)"
+        'User-Agent': 'python:alx_topten:v1.0 (by JeanPhilippe2025)'
     }
+    subreddit_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers, allow_redirects=False)
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        json_data = response.json()
+        posts = json_data.get('data', {}).get('children', [])
 
-        if response.status_code == 200:
-            data = response.json()
-            posts = data.get('data', {}).get('children', [])
-
-            for i, post in enumerate(posts):
-                if i >= 10:
-                    break
-                title = post.get('data', {}).get('title')
-                if title:
-                    print(title)
-        else:
-            print(None)
-
-    except requests.exceptions.RequestException as e:
-        print(None)
-
-    except ValueError:
+        for i in range(min(10, len(posts))):
+            print(posts[i].get('data', {}).get('title'))
+    else:
         print(None)
