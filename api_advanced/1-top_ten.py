0de1046ea700e38523f0f833409f4d7aaa6dbe10
cyuzuzo-aime <a.cyuzuzo2@alustudent.com>
2025-07-15 13:01:54 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,25 +1,20 @@
-#!/usr/bin/python3
-""" Get the titles of the first 10 hot posts for a given subreddit."""
 import requests
 
-
 def top_ten(subreddit):
+    """Return number of subscribers if @subreddit is valid subreddit.
+    if not return 0."""
 
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers, allow_redirects=False)
+    hdrs = {
+        'User-Agent': 'python:alx_topten:v1.0 (by newuser2010)'
+    }
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url, headers=hdrs, allow_redirects=False)
 
     if response.status_code == 200:
         json_data = response.json()
-        if json_data.get('data') is None or 'children' not in json_data.get('data'):
-            print(None)
-            return
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
+        posts = json_data.get('data', {}).get('children', [])
+
+        for i in range(min(10, len(posts))):
+            print(posts[i].get('data', {}).get('title'))
     else:
         print(None)
