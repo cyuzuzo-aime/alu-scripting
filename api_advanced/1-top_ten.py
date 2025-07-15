8066586409ac322537051d8ba801eaef5ea7a92e
cyuzuzo-aime <a.cyuzuzo2@alustudent.com>
2025-07-15 13:55:49 +0200

    update to use an argument

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Script that fetch 10 hot post for a given subreddit."""
 import requests
+import sys
 
 
 def top_ten(subreddit):
@@ -13,25 +14,14 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    # if response.status_code == 200:
-    #     json_data = response.json()
-    #     posts = json_data.get('data', {}).get('children', [])
-
-    #     for i in range(min(10, len(posts))):
-    #         return posts[i].get('data', {}).get('title')
-    # else:
-    #     return None
     if response.status_code == 200:
         json_data = response.json()
-        children = json_data.get('data', {}).get('children')
-        if not children:
-            return "OK"
-            return None
-        print('OK')
-        return "OK"
-        return [post.get('data', {}).get('title') for post in children[:10]]
+        posts = json_data.get('data', {}).get('children', [])
+        for i in range(min(10, len(posts))):
+            print(posts[i].get('data', {}).get('title'))
     else:
-        return "OK"
-        return None
+        print(None)
 
-top_ten("SiloSeriesdddd")
+
+if __name__ == "__main__":
+    top_ten(sys.argv[1])
