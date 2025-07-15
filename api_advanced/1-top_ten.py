c0f94f1562a8a713486a851839701990c63bb38a
cyuzuzo-aime <a.cyuzuzo2@alustudent.com>
2025-07-15 12:49:06 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,6 +4,7 @@ import requests
 
 
 def top_ten(subreddit):
+    """ Function to fetch 10 posts of a sub reddit """
     headers = {'User-Agent': 'MyAPI/0.0.1'}
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
 
